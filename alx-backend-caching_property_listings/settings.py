e36daaedc4287da7d147ca6c034eb9e348907e09
ranabadawy-02 <ranamohsen1102@gmail.com>
2026-01-18 14:48:42 +0300

    Update settings.py

--- alx-backend-caching_property_listings/settings.py
+++ alx-backend-caching_property_listings/settings.py
@@ -9,3 +9,22 @@ INSTALLED_APPS = [
     'django_redis',
     'properties',
 ]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'property_db',
+        'USER': 'property_user',
+        'PASSWORD': 'property_password',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
